d7333ce399734ad1dab348cfa2bb3651db888bbd
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-30 11:08:46 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
-sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/dkango-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/django-wsgi
 sudo service gunicorn restart
-# gunicorn -c /etc/gunicorn.d/test-wsgi hello:app
+gunicorn -c /etc/gunicorn.d/test-wsgi hello:app
+gunicorn -c /etc/gunicorn.d/django-wsgi ask.wsgi:app
