0b2801a6ec37f305383d713fdbb8f5946c6533ea
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-30 11:20:43 +0200

    Update init.sh. Change gunicorn service.

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
+sudo service gunicorn restart
 sudo gunicorn -c /home/box/web/etc/gunicorn.conf hello:wsgi_application
 sudo gunicorn -c /home/box/web/etc/gunicorn-django.conf ask.wsgi:application
-sudo service gunicorn restart
