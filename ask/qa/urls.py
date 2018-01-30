06bac225f73e0a87569d16f7131bcce72dee5799
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-30 11:05:29 +0200

    update urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,11 +3,11 @@ from .views import test
 
 
 urlpatterns = [
-    url(r'^/$', test),
-    url(r'^/login/$', test),
-    url(r'^/signup/$', test),
-    url(r'^/question/(?P<id>\d+)/$', test),
-    url(r'^/ask/$', test),
-    url(r'^/popular/$', test),
-    url(r'^/new/$', test),
+    url(r'^$', test),
+    url(r'^login/$', test),
+    url(r'^signup/$', test),
+    url(r'^question/(?P<id>\d+)/$', test),
+    url(r'^ask/$', test),
+    url(r'^popular/$', test),
+    url(r'^new/$', test),
 ]
