18e7cbd67a9c9e38504c5a7da7cdad4d55d89411
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-30 13:19:15 +0200

    Update init.sh. Change ask.wsgi:app to ask.wsgi:application

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/django-wsgi
 sudo service gunicorn restart
 gunicorn -c /etc/gunicorn.d/test-wsgi hello:app
-gunicorn -c /etc/gunicorn.d/django-wsgi ask.wsgi:app
+gunicorn -c /etc/gunicorn.d/django-wsgi ask.wsgi:application
