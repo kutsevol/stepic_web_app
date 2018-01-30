234565005f7e0b1fc5198619afef370e30c540dd
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-01-30 10:55:40 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo service nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/dkango-wsgi
 sudo service gunicorn restart
-gunicorn -c /etc/gunicorn.d/test-wsgi hello:app
+# gunicorn -c /etc/gunicorn.d/test-wsgi hello:app
