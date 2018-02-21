ff45c5dd46c8bcb03aae65fce21495bba99acf45
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-21 19:02:13 +0200

    Create 2 models: Question and Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User)
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
+    author = models.ForeignKey(User)
