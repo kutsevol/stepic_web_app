cefb158ce3618d8d7bed4a196c18d392842eb676
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-21 19:58:32 +0200

    Failed #4. Add default in IntegerField

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -14,7 +14,7 @@ class Question(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
     objects = QuestionManager()
