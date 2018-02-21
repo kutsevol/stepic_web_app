60a29dd6ba8c0b55b0eb1bfa84990c4f96bed661
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-21 19:51:58 +0200

    Add QuestionManager. Update added_at

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -6,14 +6,23 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     rating = models.IntegerField()
     author = models.ForeignKey(User)
     likes = models.ManyToManyField(User, related_name='likes_set')
+    objects = QuestionManager()
 
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateTimeField()
+    added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
