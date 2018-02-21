8a683112eef0ced51e2109a8222000d774518170
Artur Kutsevol <arthur.kutsevol@gmail.com>
2018-02-21 19:57:07 +0200

    Fix NameError

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,14 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-# Create your models here.
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
+
 class Question(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
@@ -18,11 +25,3 @@ class Answer(models.Model):
     added_at = models.DateTimeField(auto_now_add=True)
     question = models.ForeignKey(Question)
     author = models.ForeignKey(User)
-
-
-class QuestionManager(models.Manager):
-    def new(self):
-        return self.order_by('-added_at')
-
-    def popular(self):
-        return self.order_by('-rating')
